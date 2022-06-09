05bfcea05c37ccf6667367d3e63cfc3419edc87f
sikandersultan <sikandersultanmirza@gmail.com>
2022-06-09 13:16:16 +0100

    pls help

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const COLUMN_NAMES = {
+    DO_IT: 'Ingredients',
+    IN_PROGRESS: 'Plate'
+}
---
+++ src/tasks.js
@@ -0,0 +1,10 @@
+import { COLUMN_NAMES } from "./constants";
+
+const {DO_IT} = COLUMN_NAMES;
+export const tasks = [
+    {id: 1, name: 'Item 1', column: DO_IT, ingredient: 'cheese'},
+    {id: 2, name: 'Item 2', column: DO_IT, ingredient: 'chicken'},
+    {id: 3, name: 'Item 3', column: DO_IT, ingredient: 'onion'},
+    {id: 4, name: 'Item 4', column: DO_IT, ingredient: 'salt'},
+    {id: 5, name: 'Item 5', column: DO_IT, ingredient: 'pepper'},
+];
